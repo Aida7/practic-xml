11b41f9b124b4362e232414c9370e38706a8e50c
Садыкова Аида Хаирбекова <@corp.itstep>
2017-12-22 11:36:38 +0600

    practic xml

---
+++ XMLDocument/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLDocument
+{
+    public class Book
+    {
+     public Guid id { get; set; }
+     public string Nmae { get; set; }
+     public string Author { get; set; }  
+    }
+}
---
+++ XMLDocument/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace XMLDocument
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load("Teg.xml");
+            var root = document.DocumentElement;
+            foreach (XmlNode child in root.ChildNodes)
+            {
+                Console.WriteLine("{0}   ", child.LocalName);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
